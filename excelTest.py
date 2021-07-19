fdc7b7a29f62179816196e9e48eb211c1dc4744c
mrjamesbruno <mriurii@yahoo.com>
2021-07-19 13:56:00 +0300

    excel Format for horoshop

---
+++ excelTest.py
@@ -0,0 +1,39 @@
+﻿# -*- coding: utf-8 -*-
+#dlya russkogo nujno sohranyat .py v UTF-8
+import requests
+import urllib.request
+from urllib.request import Request, urlopen
+import time
+from bs4 import BeautifulSoup
+import csv
+import pdb
+import re
+import xlsxwriter
+
+num = [1,2,3,4,5,6,7,8,9,0]
+#all_products  <class 'list'>
+all_products = []
+all_products.append(['Артикул','Название (RU)','Цена','Описание товара (RU)'])
+all_products.append([num[0],num[1],num[2],num[3]])
+all_products.append([num[4],num[5],num[6],num[7]])
+
+"""
+#expenses:  <class 'tuple'>  
+expenses = (
+     ['Rent', '2013-01-13', 1000],
+     ['Gas',  '2013-01-14',  100],
+     ['Food', '2013-01-16',  300],
+	)
+"""
+print("all_products: ",type(all_products))
+#print("expenses: ",type(expenses))
+
+workbook = xlsxwriter.Workbook('test.xlsx')
+worksheet = workbook.add_worksheet()
+for row_num, row_data in enumerate(all_products):
+    print("row_num, row_data: ",row_num,row_data)
+    for col_num, col_data in enumerate(row_data):
+        print(">>>row_num, col_num, col_data: ",row_num,col_num,col_data)
+        worksheet.write(row_num, col_num, col_data)
+        
+workbook.close()
---
+++ extractLinks3.py
@@ -0,0 +1,142 @@
+﻿# -*- coding: utf-8 -*-
+import requests
+import urllib.request
+from urllib.request import Request, urlopen
+import time
+from bs4 import BeautifulSoup
+import csv
+import pdb
+import re
+import xlsxwriter
+
+headers1='User-Agent'
+headers2 = 'Mozilla/5.0'
+all_products = []
+all_products.append(["Артикул","Название (RU)","Цена","Описание товара (RU)"])
+all_productsXLSX = []
+all_links = []
+artikul = "000"
+indexOfImg = 1
+req = Request('https://brig-motors.com/product-category/vodnolyzhnoje-snaryazhenije/zhilety/page/2/', headers={headers1: headers2})
+webpage = urlopen(req).read()
+soup = BeautifulSoup(webpage, 'html.parser')
+
+file = open('links.txt', 'w')
+webpageLinks = soup.select('div.stm-product-inner')
+for links in webpageLinks:
+	for link1 in links.findAll('a'):
+		all_links.append(link1['href'])
+		file.write(str(link1['href'])+"\n")
+file.close()
+
+"""
+#read file
+f = open("links.txt", "r")
+for x in f:
+  print(x)
+"""
+def getImgUrl(imageName):
+	#print("getImgUrl: ", len(imageName),imageName[0])
+	rightName = ''
+	imageName = imageName[:-1]
+	#print("getImgUrl2: ", len(imageName),imageName[0])
+	for y in imageName:
+		#print(y)
+		rightName += y+"-"
+	return rightName[:-1]+".jpg"
+
+def glueURL(url0):
+	urlBack = ''
+	#print("glueURL: ", len(url0),url0[0])
+	url0 = url0[:-1]
+	#del record[-1]
+	#print("glueURL2: ", len(url0),url0[0])
+	for y in url0:
+		urlBack += y+"/"
+	#print("glueURL: urlBack: ", urlBack)
+	return str(urlBack)
+
+for link in all_links:
+	indexOfImg = 1
+	#print(link)
+	req = Request(link, headers={headers1: headers2})
+	webpage = urlopen(req).read()
+	soup = BeautifulSoup(webpage, 'html.parser')
+	products = soup.select('div.col-md-9.col-md-push-3.col-sm-12')
+	for product in products:
+		##product-9773 > div.row > div.col-lg-7.col-md-7.col-sm-12.col-xs-12 > div > h1
+	    name = product.select('h1.product_title.entry-title')[0].text.strip()
+	    print(name)
+	    #breakpoint()
+	    #true_value if condition else false_value
+	    d = product.select('div.woocommerce-product-details__short-description > p')
+	    description = (d[0].text.strip() if len(d) else "empty")
+	    print(description)
+	    price = product.select('span.woocommerce-Price-amount.amount > bdi')[0].text.strip()
+	    print(price)
+	    artikul = product.select('span.sku')[0].text.strip()
+	    print(artikul)
+	    
+	    all_products.append([artikul, name, price, description])
+	
+	
+	img_tags = soup.find_all('img')
+	urls = [img['src'] for img in img_tags]
+	print("found img: ",len(urls))
+	for url in urls:
+		#print(url)
+		filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+		if not filename:
+		    #print("Regex didn't match with the url: {}".format(url))
+		    continue
+		#print(filename.group(1))
+		#with open(filename.group(1), 'wb') as f:
+		x = url.split("/")
+		x1 = x[len(x)-1]
+		#print("artikul i x1: ", artikul, x1)
+		if artikul in x1:
+		    #print("artikul in x1 >>>>>>>>>>>>>>")
+		    urlWithoutIMG = glueURL(x)
+		    #imya sohranyaemogo fila
+		    imgName = artikul+"@"+str(indexOfImg)+".jpg"
+		    #print("imgName: ",imgName)
+		    #sdelat pravilnyi webadres kartinki
+		    xName0 = x1.split("-")
+		    #print("xName0: ",xName0[0])
+		    xName = getImgUrl(xName0)
+		    #print("xName: ",xName)
+		    #udalyaem imya img i zamenyaem vernym xName
+		    url = urlWithoutIMG+str(xName)
+		    print("url: ",url)
+		    try:
+			    opener = urllib.request.build_opener()
+			    opener.addheaders = [(headers1, headers2)]
+			    urllib.request.install_opener(opener)
+			    urllib.request.urlretrieve(url, "IMG\\"+imgName)
+		    except:
+		    	print("!!! ERROR: Ne mogu skachat kartinky! >>>> ", url)
+		    indexOfImg +=1
+	
+	#breakpoint()
+
+#https://xlsxwriter.readthedocs.io/working_with_data.html
+workbook = xlsxwriter.Workbook('brig-motors.xlsx')
+worksheet = workbook.add_worksheet()
+for row_num, row_data in enumerate(all_products):
+    for col_num, col_data in enumerate(row_data):
+        worksheet.write(row_num, col_num, col_data)
+
+# Write a total using a formula.
+#worksheet.write(row, 0, 'Total')
+#worksheet.write(row, 1, '=SUM(B1:B4)')
+
+workbook.close()
+
+"""
+keys = all_products[0].keys()
+
+with open('products.csv', 'a', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(all_products)
+"""
